48c8f3368006c22143e6c35660151e3693f02bed
Adam Beckmeyer <adam_gpg@thebeckmeyers.xyz>
2017-07-21 21:54:10 -0400

    Add logging and debug mode

--- mpp.py
+++ mpp.py
@@ -13,6 +13,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import logging
+import sys
 from gyr import server, matrix_objects, exceptions
 from pyvirtualdisplay.smartdisplay import SmartDisplay
 from easyprocess import EasyProcess
@@ -20,16 +22,29 @@ import time
 import json
 from io import BytesIO
 
+handler = logging.StreamHandler(sys.stdout)
+formatter = logging.Formatter("%(asctime)s : %(levelname)s : %(name)s : %(message)s")
+handler.setFormatter(formatter)
+logger = logging.getLogger()
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
+
 # Must start display before we can use Xlib via pynput
 disp = SmartDisplay(visible=False, size=(240, 160))
 disp.start()
+logger.info("xvfb display started")
 from pynput.keyboard import Key, Controller
-# Have to start pulse here because computer is being a weenie
+logger.info("pynput imported and working")
+# Have to start pulse here because mgba is being a weenie
 pulse = EasyProcess("pulseaudio")
 pulse.start()
+logger.info("pulseaudio started")
 
 with open("config.json") as f:
     config = json.load(f)
+
+if config["debug"]:
+    logger.setLevel(logging.DEBUG)
 
 application = server.Application(config["hs_address"], config["token"])
 
@@ -43,18 +58,26 @@ class MPPServer:
                                   ":" + self.config["hs_name"])
 
         self.disp = disp
-        self.pkmn = EasyProcess("mgba -s 2 -b " + self.config["bios_location"] + " " + self.config["rom_location"])
-        self.pkmn.start()
-        self.keyboard = Controller()
+        self._start_mgba()
 
+        self.keyboard = Controller()
         self.api = application.Api()
         try:
             # when creating a room with a room alias, only local part of alias is used
             self.room_id = self.api.create_room(alias=self.config["local_room_alias"],
                                                 is_public=True)
+            logger.info("new room created: " + self.room_id)
         except exceptions.MatrixError:
             # if it already exists just get the room_id
             self.room_id = self.api.get_room_id(self.config["room_alias"])
+            logger.info("using existing room: " + self.room_id)
+
+    def _start_mgba(self):
+        self.pkmn = EasyProcess("mgba -s 2 -b " +
+                                self.config["bios_location"] + " " +
+                                self.config["rom_location"])
+        self.pkmn.start()
+        logger.info("mgba started")
 
         self.ts = time.time()
         time.sleep(5)
@@ -78,24 +101,30 @@ class MPPServer:
             mxc = self.api.media_upload(f.getvalue(), "image/jpeg")["content_uri"]
             file_name = str(int(self.ts)) + ".jpg"
             self.api.send_content(self.room_id, mxc, file_name, "m.image")
+            logger.debug("sent screenshot to " + self.room_id)
         except AttributeError:
             self.api.send_notice(self.room_id, "Error in capturing screenshot")
+            logger.error("could not capture screenshot from display")
 
     def _save(self):
         if self.ts:
             if self.ts + 100 < time.time():
                 with keyboard.pressed(Key.shift):
                     self._press_key(Key.f1)
+                logger.debug("saved current gamestate to f1")
 
     def _load(self):
         self._press_key(Key.f1)
+        logger.debug("loaded gamestate from f1")
 
     def room_handler(self, room_alias):
         # Only room created is #matrixplayspokemon
+        logger.info("homeserver asked for " + room_alias)
         return room_alias == self.config["room_alias"]
 
     def user_handler(self, mxid):
         # Only user is as_user.mxid
+        logger.info("homeserver asked for " + mxid)
         return mxid == self.config["user_id"]
 
     def transaction_handler(self, event_stream):
@@ -124,6 +153,22 @@ class MPPServer:
                     self._press_key(Key.enter)
                 elif content == "select":
                     self._press_key(Key.backspace)
+
+                elif content == "dump" and self.config["debug"]:
+                    logger.debug("received dump command")
+                    self._dump()
+                elif content == "save" and self.config["debug"]:
+                    logger.debug("received save command")
+                    with keyboard.pressed(Key.shift):
+                        self._press_key(Key.f1)
+                    logger.debug("saved current gamestate to f1")
+                elif content == "load" and self.config["debug"]:
+                    logger.debug("received load command")
+                    self._load()
+
+            logger.debug("handled " + event.type +
+                         " from " + event.mxid +
+                         " in " + event.id)
             self.send_screenshot()
             self._save()
         return True
@@ -133,8 +178,16 @@ class MPPServer:
         # make sure the key is pressed long enought to register with mgba
         time.sleep(0.05)
         self.keyboard.release(key)
+        logger.debug("pressed key: " + str(key))
+
+    def _dump(self):
+        self.pkmn.stop()
+        self.warning("mgba stdout: " + self.pkmn.stdout)
+        self.warning("mgba stderr: " + self.pkmn.stderr)
+        self._start_mgba()
 
 mpp = MPPServer(application, config)
+logger.info("setup complete")
 application.add_handlers(room_handler=mpp.room_handler,
                          transaction_handler=mpp.transaction_handler,
                          user_handler=mpp.user_handler,)
